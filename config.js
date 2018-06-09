57362b5a07e4d9705e43a97f58a48afa21e17d9e
xmdhs <xmdhs@users.noreply.github.com>
2018-06-09 10:02:25 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+config = {
+    blog_name: "xmdhs",
+    github_username: "xmdhs", // github 账号名
+    github_repo: "xmdhs.github.io",      // github 建立的 repo 名
+    per_page: 10,             // 首页每次载入文章列表数量
+
+    // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
+    // 将配置好的 access token 拆分成两个字符串相加
+    access_token: "ca957d693e774c4433f4d"+"9c2914aa3608353372d"
+};
