9ce685a9495fbc441872b37369d984dd4982a1eb
xmdhs <xmdhs@users.noreply.github.com>
2018-06-22 22:05:58 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ config = {
 
     // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
     // 将配置好的 access token 拆分成两个字符串相加
-    access_token: "17c740f8563c164e7a6d+2600b821bdd49f86a5eb"
+    access_token: "17c740f8563c164e7a6d"+"2600b821bdd49f86a5eb"
 };
